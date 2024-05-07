91d49a5a9d6a1907a9f17df5540fd21c987ecb40
plOloV2 <ak.dziagwa@gmail.com>
2024-05-07 15:08:41 +0200

    more work on lcd

--- include/LCD.h
+++ include/LCD.h
@@ -18,70 +18,76 @@
 
     8 data pins mode
     16x2 display mode
+    
 */
 
-void pulse_enable(){        //wait command for LCD to proces instruction
+void pulse_enable(){          //wait command for LCD to proces instruction
 
-  PORTD |= 0b00100000;      //sets EN pin to HIGH
+  PORTD |= 0b00100000;        //sets EN pin to HIGH
 
-  delayMicroseconds(1);     //enable pulse must be >450ns
+  delayMicroseconds(1);       //enable pulse must be >450ns
 
-  PORTD &= !0b00100000;     //sets EN to LOW -> LCD begins executing instructions
+  PORTD &= !0b00100000;       //sets EN to LOW -> LCD begins executing instructions
 
-  delayMicroseconds(100);   // commands need >37 us to settle
+  delayMicroseconds(100);     // commands need >37 us to settle
+
 }
 
-void LCD_setup_1(){       //resets LCD into listening to setup commands, must be  3 times
+void LCD_setup_1(){           //resets LCD into listening to setup commands, must be 3 times
 
-  DDRD |= 0b11110000;     //sets pins 4-13 as output
+  DDRD |= 0b11110000;         //sets pins 4-13 as output
   DDRB |= 0b00111111;
 
-  PORTD &= !0b11110000;   //sets pins 4-7 as LOW 
-  PORTB &= !0b00111111;   //sets pins 8-13 as LOW
+  PORTD &= !0b11110000;       //sets pins 4-7 as LOW 
+  PORTB &= !0b00111111;       //sets pins 8-13 as LOW
 
-  PORTD &= !0b10110000;   //sets pins 4,6,7 as LOW
-  PORTB |= 0b00000011;    //sets pins 8 and 9 to HIGH
+  PORTB |= 0b00000111;        //sets pins 8, 9, 10 to HIGH
   
   pulse_enable();
 }
 
-void LCD_setup_2(){       //sets 8pin mode and display & font size
-  
-  PORTD &= !0b10110000;   //sets pins 4,6,7 as LOW
-  PORTB |= 0b00000111;    //sets pins 8, 9, 10 to HIGH
-  PORTB &= !0b00001000;   //sets pins 11 to LOW
+void LCD_setup_2(){           //turns display on
+
+  PORTD &= !0b11110000;       //sets pins 4-7 as LOW
+  PORTB |= 0b00001000;        //sets pins 11 to HIGH
+  PORTB &= !0b00110111;       //sets pins 8-10, 12, 13 to LOW
 
   pulse_enable();
 }
 
-void LCD_setup_3(){       //turn display and cursor on
+void LCD_setup_3(){           //sets display to write from left to right
 
-  PORTD &= !0b10110000;   //sets pins 4,6,7 as LOW
-  PORTB |= 0b00011100;    //sets pins 10,11,12 to HIGH
-  PORTB &= !0b00100011;   //sets pins 8, 9, 13 to LOW
-
-  pulse_enable();
-}
-
-void LCD_setup_4(){       //sets display to write from left to right
-
-  PORTD |= 0b00100000;    //sets EN pin to HIGH
-  PORTD &= !0b10110000;   //sets pins 4,6,7 as LOW
-  PORTB |= 0b00011000;    //sets pins 11,12 to HIGH
-  PORTB &= !0b00100111;   //sets pins 8, 9, 10, 13 to LOW
+  PORTD &= !0b11110000;       //sets pins 4-7 as LOW
+  PORTB |= 0b00011000;        //sets pins 11,12 to HIGH
+  PORTB &= !0b00100111;       //sets pins 8-10, 13 to LOW
 
 }
 
-void LCD_write(char letter, bool * ){
+void LCD_write(char letter){
 
 }
 
 void set_row(unsigned char row){
+  if(row == 1){
 
+  }
+  else{
+
+  }
+
+  pulse_enable();
 }
 
 void lcd_clear(){
 
+  PORTD &= !0b11110000;       //sets pins 4-7 as LOW 
+  PORTB &= !0b00011111;       //sets pins 8-12 as LOW
+
+  PORTB |= 0b00100000;        //sets pins 13 to HIGH
+
+  pulse_enable();
+
+  delayMicroseconds(2000);    //takes a LONG time
 }
 
 
@@ -97,16 +103,28 @@ void lcd_setup(){             //initializes LCD page 45, figure 23
 
   LCD_setup_1();              //third time is a charm
 
-  LCD_setup_2();              //display mode setup first part
+  LCD_setup_1();              //sets 8pin mode, display & font size
 
-  LCD_setup_3();              //display mode setup second part
+  LCD_setup_2();              //turns display on
 
-  LCD_setup_4();              //display mode setup third part
+  lcd_clear();                //clears screen
+
+  LCD_setup_3();              //sets display to write from left to right
 
 }
 
 
 void lcd_print(String row1, String row2){
+  
+  lcd_clear();
+
+  set_row(1);
+
+  for(unsigned char i = 0; i < 16; i++){
+
+  }
+
+  set_row(2);
 
   for(unsigned char i = 0; i < 16; i++){
 
